57b670dc9b4dae005222fb5cdd75fe3e85c68cda
sselzer <sascha.selzer@innoq.com>
2023-02-05 15:53:48 +0100

    Add second endpoint

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
@@ -27,6 +29,7 @@ func main() {
 	router.Use(otelgin.Middleware(APP_NAME))
 
 	router.GET("/", HelloHandler)
+	router.GET("/random", RandomHandler)
 
 	logrus.Info("Start listening on port 8080")
 
@@ -37,4 +40,12 @@ func main() {
 func HelloHandler(c *gin.Context) {
 	logrus.WithContext(c.Request.Context()).Info("Call hello endpoint")
 	c.String(http.StatusOK, "Hello '%s'", APP_NAME)
+}
+
+func RandomHandler(c *gin.Context) {
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(10)
+	logrus.WithContext(c.Request.Context()).Infof("Call random endpoint and wait for %d seconds", n)
+	time.Sleep(time.Duration(n) * time.Second)
+	c.String(http.StatusOK, "From random")
 }
